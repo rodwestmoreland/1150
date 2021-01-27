a4e6a18e064ada0c09c852b98c24fcd6217ea526
Rod Westmoreland <rcwest@gmail.com>
2021-01-27 09:32:45 -0500

    morning assignment

---
+++ SDLessonsB/03_Conditions/Conditions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _03_Conditions
+{
+    [TestClass]
+    public class Conditions
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int count=0;
+            string text = "Supercalifragilisticexpialidocious";
+
+            foreach (char letter in text)
+            {
+                count++;
+                Console.WriteLine(letter);
+            }
+
+            Console.WriteLine(count);
+
+            for (int i = 0; i<text.Length; i++)
+            {
+                //if (text[i] == 'i' || text[i] == 'l')
+                if(text[i]=='i')
+                {
+                    Console.WriteLine($"found an i: {text[i]}");
+                }
+                else if (text[i]=='l')
+                {
+                    Console.WriteLine($"found an L: {text[i]}");
+                }
+
+            }
+        }
+    }
+}
